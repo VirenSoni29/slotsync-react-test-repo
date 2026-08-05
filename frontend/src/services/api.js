b874e88e1f3e1612deca88d1873d6ed82b1ae3d6
Viren Soni <71062471+VirenSoni-Dev@users.noreply.github.com>
2026-08-06 00:01:08 +0530

    Added .env in frontend for backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { clearAccessToken, getAccessToken, setAccessToken } from './tokenService.js';
 
 const api = axios.create({
-   baseURL: 'http://localhost:5000/api',
+   baseURL: import.meta.env.VITE_BACKEND_API_URL,
    withCredentials: true,
    headers: {
       'Content-Type': 'application/json'
